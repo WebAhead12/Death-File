892b2b54efa6e10ae4ef0d10f49de356445a9aea
Nur1995 <79867892+Nur1995@users.noreply.github.com>
2021-11-02 12:17:50 +0200

    Delete back_end directory

--- back_end/server.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const router = require("./router");
-
-const server = http.createServer(router);
-
-server.listen(5000, () => console.log(`Listening at http://localhost:5000`));
