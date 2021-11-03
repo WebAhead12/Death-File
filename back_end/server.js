f001d1ab78ac25def68342526adca8eb1d897b1e
julio1703 <julio.elias22@gmail.com>
2021-11-03 12:25:44 +0200

    updated script.js

--- back_end/server.js
+++ back_end/server.js
@@ -3,4 +3,4 @@ const router = require("./router");
 
 const server = http.createServer(router);
 
-server.listen(5000, () => console.log(`Listening at http://localhost:5000`));
+server.listen(9898, () => console.log(`Listening at http://localhost:9898`));
--- front_end/script.js
+++ front_end/script.js
@@ -1,12 +1,25 @@
 const searchinput = document.querySelector("#search-input");
+const searchresult = document.querySelector("#search-results");
 searchinput.addEventListener("keyup", () => {
+  let i = 0;
+  let response = getAnimeNames();
+  console.log(response);
+  searchresult.innerHTML = "";
+  while (i < response.length) {
+    let resultbox = document.createElement("span");
+    resultbox.classList.add("result-box");
+    resultbox.textContent = response[i];
+    resultbox.addEventListener("click", () => {});
+  }
+});
+function getAnimeNames() {
   fetch(`data/${searchinput.value}`)
     .then((response) => {
       if (!response.ok) throw new Error(response.status);
-      response.json();
+      return response.json();
     })
-    .then((response) => {})
     .catch((error) => {
       console.error(error);
     });
-});
+}
+// result-box (class name for earch results)
