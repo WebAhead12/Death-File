e2dda6ae52b842ed4fef54973cbc7fe93b55de36
julio1703 <julio.elias22@gmail.com>
2021-11-03 10:05:01 +0200

    Update data.js

--- Handlers/data.js
+++ Handlers/data.js
@@ -1,18 +1,26 @@
 const fs = require("fs");
 const path = require("path");
 const dataObject = require("../back_end/dataObject.js");
-const AUTOCOMPLETE_LENGTH = 8;
+// const AUTOCOMPLETE_LENGTH = 8;
 function dataHandler(request, response) {
   console.log(request.url);
   const url = request.url;
   const urlArray = url.split("/");
   const value = urlArray[2];
   const filterArray = [];
+  let counter = 0;
   dataObject.forEach((element) => {
-    if (element["name"].toLowerCase().indexOf(value.toLowerCase()) === 0) filterArray.push(element["name"]);
+    if (element["name"].toLowerCase().indexOf(value.toLowerCase()) === 0) {
+      if (counter !== 8) {
+        filterArray.push(element["name"]);
+        counter++;
+      }
+    }
   });
   console.log(filterArray);
   response.writeHead(200, { "content-type": "application/json" });
-  response.end(JSON.stringify(filterArray.splice(0, Math.min(AUTOCOMPLETE_LENGTH, filterArray.length))));
+  // response.end(JSON.stringify(filterArray.splice(0, Math.min(AUTOCOMPLETE_LENGTH, filterArray.length))));
+  response.end(JSON.stringify(filterArray));
 }
+
 module.exports = dataHandler;
